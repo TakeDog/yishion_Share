8fd96b0f451632c8befce3c6dd395809946ff708
YeJianFa <1205648736@qq.com>
2019-08-15 18:05:55 +0800

    论坛管理&公司资讯

--- app/portal/controller/InfoController.php
+++ app/portal/controller/InfoController.php
@@ -9,5 +9,29 @@ class InfoController extends HomeBaseController{
         return $this -> fetch();
     }
 
+    public function getInfo(){
+        $where = $this -> request -> param();
+        
+        
+        if($where['id'] == -1){
+            $data = Db::name('WorkInfo') -> where('files','') -> order('sort') -> select();
+        }else{
+            $data = Db::name('WorkInfo') -> where('id',$where['id']) -> order('sort') -> select();
+        }
+        return json($data);
+    }
+
+    public function getFiles(){
+        $pid = $this -> request -> param("pid",0,"intval");
+        $page = $this -> request -> param("page",1,"intval");
+        $size = $this -> request -> param("size",5,"intval");
+        
+        $current = ($page-1)*$size;
+
+        $data = Db::name('WorkInfo') -> where('pid',$pid) -> order('sort') -> limit($current,$size) -> select();
+
+        return json($data);
+    }
+
    
 }
